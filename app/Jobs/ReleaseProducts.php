6eeb2b09fbf37c164e50c2a7042242dd6e10fcae
lioDenson <codensons@gmail.com>
2026-04-25 12:54:52 +0300

    perf: A lock method to prevent concurency on updating the order status  by both queue and schedular

--- app/Jobs/ReleaseProducts.php
+++ app/Jobs/ReleaseProducts.php
@@ -7,6 +7,7 @@ use App\Repositories\Inventory\BatchRepository;
 use App\Repositories\OrderRepository;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class ReleaseProducts implements ShouldQueue
@@ -30,27 +31,39 @@ class ReleaseProducts implements ShouldQueue
      */
     public function handle(OrderRepository $orderRepository, BatchRepository $batchRepository): void
     {
-        $order = $orderRepository->findByOrderNumber($this->orderNumber);
+        $lockKey = 'release-order-'. $this->orderNumber;
+
+        $lock = Cache::lock($lockKey, 10);
+
+       if($lock->get()) {
+        try{
+            $order = $orderRepository->findByOrderNumber($this->orderNumber);
 
         
 
-        if(
-            !$order || 
-            in_array($order->status, ['expired','completed']) || 
-            is_null( $order->expires_at) ||  
-            $order->expires_at->isFuture()
-        ){
-            return;
-        }
+            if(
+                !$order || 
+                in_array($order->status, ['expired','completed']) || 
+                is_null( $order->expires_at) ||  
+                $order->expires_at->isFuture()
+            ){
+                return;
+            }
 
 
-        DB::transaction(function () use($order, $orderRepository, $batchRepository){
-            $orderRepository->update($order->id, ['status' => 'expired','expires_at' => null]);
+            DB::transaction(function () use($order, $orderRepository, $batchRepository){
+                $updated = $orderRepository->update($order->id, ['status' => 'expired','expires_at' => null]);
+                if(!$updated){
+                    return;
+                }
 
-            $order->items->each(function ($item) use($batchRepository){
-                $batchRepository->releaseStock($item->batch_id,  $item->quantity);
-            });
-        },2);
+                $order->items->each(function ($item) use($batchRepository){
+                    $batchRepository->releaseStock($item->batch_id,  $item->quantity);
+                });
+            },2);
+        }finally{
+            $lock->release();
+        }}
 
         
     }
