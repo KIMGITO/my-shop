c9c6c0e4f62ebc7019e43cf91a2ad9637753a839
lioDenson <codensons@gmail.com>
2026-04-28 10:33:32 +0300

    merged stash with current changes

---
+++ app/Models/Catalog.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Catalog extends Model
+{
+    protected $appends = ['categories'];
+
+    public function getCategoriesAttribute(){
+        
+    }
+}
