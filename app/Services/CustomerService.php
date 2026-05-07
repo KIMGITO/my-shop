d8807ffe706b5f887ec083b531a29cf47f5cd214
lioDenson <codensons@gmail.com>
2026-05-07 11:43:02 +0300

    fix:  checking thepayment page

--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -20,7 +20,7 @@ class CustomerService
     public function transactions(int $limit = 5){
         $payments = $this->customerRepository->latestPayments($limit);
 
-        dd($payments);
+        // dd($payments);
 
     }
 }
